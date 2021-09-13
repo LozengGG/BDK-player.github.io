37909c109aed44d4b35c36e6a85353a05d64b464
Yuwei Wang <yuwei.wang.acbel@gmail.com>
2021-09-14 04:20:25 +0800

    Basic filter function. Collapse filter.

--- src/main.js
+++ src/main.js
@@ -4,18 +4,55 @@ import $ from 'jquery';
 import _ from 'lodash';
 
 const songlist = require("../songlist.json");
-let currFilter = 'all';
+const currFilter = [];
 
 const ap = new APlayer({
     container: document.getElementById('aplayer'),
     audio: songlist,
+    listMaxHeight: 480,
 });
 
-function onSelectTag(tagName) {
-    $(`#${currFilter}`).removeClass("active");
-    $(`#${tagName}`).addClass("active");
-    $('#listTitle').text(tagName);
-    currFilter = tagName;
+function onSelectFilter(selectedFilter) {
+    if ($("#all").hasClass("active")) {
+        $("#all").removeClass("active");
+    }
+    if ($(`#${selectedFilter}`).hasClass("active")) {
+        $(`#${selectedFilter}`).removeClass("active");
+        _.remove(currFilter, filter => filter === selectedFilter);
+    } else {
+        $(`#${selectedFilter}`).addClass("active");
+        currFilter.push(selectedFilter);
+    }
+    $('#listTitle').text(currFilter.length > 1
+        ? currFilter.join(" + ") 
+        : currFilter[0]);
+    onFilterChange();
+}
+
+function onFilterChange() {
+    if (currFilter.length === 0) {
+        onSelectAll();
+        return;
+    }
+    let newSongList = [...songlist];
+    currFilter.forEach(filter => {
+        newSongList = newSongList.filter(song => {
+            return song.artist === filter || song.tags.includes(filter);
+        });
+    });
+    ap.list.clear();
+    ap.list.add(newSongList);
+}
+
+function onSelectAll() {
+    currFilter.forEach(filter => {
+        $(`#${filter}`).removeClass("active");
+    });
+    currFilter.splice(0, currFilter.length);
+    $("#all").addClass("active");
+    ap.list.clear();
+    ap.list.add(songlist);
+    $('#listTitle').text("All");
 }
 
 const [tagFilters, artistFilter] = songlist.reduce((acc, curr) => {
@@ -37,31 +74,17 @@ const [tagFilters, artistFilter] = songlist.reduce((acc, curr) => {
 tagFilters.forEach((tag, index) => {
     $("#tagFilterRow").append(`<button class="btn btn-outline-primary btn-sm ${index > 0 ? 'ms-1' : ''}" type="button" id="${tag}">${tag}</button>`);
     $(`#${tag}`).on('click', () => {
-        onSelectTag(tag);
-        const newSongList = songlist.filter(song => {
-            if (!song.tags) return false;
-            return song.tags.includes(tag);
-        });
-        ap.list.clear();
-        ap.list.add(newSongList);
+        onSelectFilter(tag);
     })
 });
 
 artistFilter.forEach((artist, index) => {
-    $("#artistFilterRow").append(`<button class="btn btn-outline-primary btn-sm ${index > 0 ? 'ms-1' : ''}" type="button" id="${artist}">${artist}</button>`);
+    $("#artistFilterRow").append(`<button class="btn btn-outline-secondary btn-sm ${index > 0 ? 'ms-1' : ''}" type="button" id="${artist}">${artist}</button>`);
     $(`#${artist}`).on("click", () => {
-        onSelectTag(artist);
-        const newSongList = songlist.filter(song => {
-            if (!song.artist) return false;
-            return song.artist.toLowerCase() === artist.toLowerCase();
-        });
-        ap.list.clear();
-        ap.list.add(newSongList);
+        onSelectFilter(artist);
     });
 });
 
 $("#all").on('click', () => {
-    onSelectTag("all");
-    ap.list.clear();
-    ap.list.add(songlist);
+    onSelectAll();
 });
