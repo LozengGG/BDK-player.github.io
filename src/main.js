83d459bd0a8f4e9e3551454220f15427ac340b53
bigjj426 <masamimitsouka@gmail.com>
2021-09-13 21:40:35 +0800

    second music

--- src/main.js
+++ src/main.js
@@ -3,10 +3,18 @@ import APlayer from '../node_modules/aplayer/dist/APlayer.min.js';
 
 const ap = new APlayer({
     container: document.getElementById('aplayer'),
-    audio: [{
-        name: 'NEXT COLOR PLANET',
-        artist: 'Suisei',
-        url: 'music/NEXT COLOR PLANET.mp3',
-        cover: 'pic/NEXT COLOR PLANET.jpg'
-    }]
+    audio: [
+        {
+            name: 'NEXT COLOR PLANET',
+            artist: 'Suisei',
+            url: 'music/NEXT COLOR PLANET.mp3',
+            cover: 'pic/NEXT COLOR PLANET.jpg'
+        },
+        {
+            name: 'GHOST',
+            artist: 'Suisei',
+            url: 'music/GHOST.mp3',
+            cover: 'pic/GHOST.jpg'
+        }
+]
 });
